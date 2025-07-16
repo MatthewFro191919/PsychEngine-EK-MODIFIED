29ec1bcae49484dd87b0f92840c51bc0f9602e81
Diebitxh <113533407+Diebitxh@users.noreply.github.com>
2025-07-17 01:26:56 +0300

    Update ErrorState.hx

--- source/states/ErrorState.hx
+++ source/states/ErrorState.hx
@@ -45,7 +45,7 @@ class ErrorState extends MusicBeatState
 		errorSine += 180 * elapsed;
 		errorText.alpha = 1 - Math.sin((Math.PI * errorSine) / 180);
 
-		if(controls.ACCEPT && acceptCallback || #if mobile || _virtualpad.buttonA.justPressed #end != null)
+		if(controls.ACCEPT #if mobile || _virtualpad.buttonA.justPressed #end && acceptCallback != null)
 			acceptCallback();
 		else if(controls.BACK && backCallback != null)
 			backCallback();
