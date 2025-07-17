c1ad62b7ff234360820f04cd9b9ebda715e6c4c5
Diebitxh <113533407+Diebitxh@users.noreply.github.com>
2025-07-17 03:46:00 +0300

    Update FreeplayState.hx

--- source/states/FreeplayState.hx
+++ source/states/FreeplayState.hx
@@ -66,7 +66,7 @@ class FreeplayState extends MusicBeatState
 			{
 				FlxTransitionableState.skipNextTransIn = true;
 				persistentUpdate = false;
-				MusicBeatState.switchState(new states.ErrorState("NO WEEKS ADDED FOR FREEPLAY\n\nPress ACCEPT to go to the Week Editor Menu.\nPress BACK to return to Main Menu.",
+				MusicBeatState.switchState(new states.ErrorState("NO WEEKS ADDED FOR FREEPLAY\n\nPress A to go to the Week Editor Menu.\nPress B to return to Main Menu.",
 					function() MusicBeatState.switchState(new states.editors.WeekEditorState()),
 					function() MusicBeatState.switchState(new states.MainMenuState())));
 				return;
